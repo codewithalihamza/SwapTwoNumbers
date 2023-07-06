b5984cf7fff61f111fbdb5d30e2d0825761b5592
Ali Hamza <59261458+codewithalihamza@users.noreply.github.com>
2023-07-06 20:22:12 +0500

    Add files via upload

---
+++ Swapnumber.js
@@ -0,0 +1,25 @@
+// Swap without new Varriable
+
+let a=2
+let b=12
+
+a=a+b         //a=14
+b=a-b       //  b=14-12=2
+
+a=a-b;  // a=12
+
+console.log(a);
+console.log(b);
+
+
+// Swap with a new varriable
+
+// let a=2
+// let b=12
+// let c=0
+// c=b
+// b=a
+// a=c
+
+// console.log(a)
+// console.log(b)
